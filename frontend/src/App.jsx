54d418d553c22c003104a8c20f7b85f3422905b0
Sebattaz <seba.lostar@gmail.com>
2024-11-28 18:39:23 -0300

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from 'react'
+import {Route, Routes} from 'react-router-dom'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import NavBar from './componets/Navbar'
+import Home from './views/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+ //const [count, setCount] = useState(0)
 
   return (
     <>
-      
+    <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
     
     </>
   )
--- frontend/src/componets/Navbar.jsx
+++ frontend/src/componets/Navbar.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import './styles/navbar.css';
+import { Nav, Navbar} from 'react-bootstrap';
+import {NavLink} from 'react-router-dom';
+import { FaSearch } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
 
-const Navbar = () => {
+
+const NavBar = () => {
+
+    const setActive = ({isActive})=>(isActive ? "active" : undefined)
+
   return (
-    <div>Navbar</div>
+    <Navbar className='bg-light d-flex align-items-center justify-content-between' expand='lg'>
+        <Navbar.Brand className='mx-5' href='/'>
+            <p>Productos</p>
+        </Navbar.Brand>
+        <img className='logo me-3' src='/src/assets/logo.jpg' alt="HomeCloset" />
+        <div className='d-flex flex-grow-1 justify-content-center'>
+            
+            <div className='Titulo'><h1 >HOME CLOSET</h1></div>
+        </div>
+        <Navbar.Toggle aria-controls='basic-navbar-var'/>
+        <Navbar.Collapse id='basic-navbar-var' className='justify-content-end'>
+            <Nav>
+                <Nav.Link as={NavLink} className={`${setActive} d-flex flex-column align-items-center text-center`} to={"/"} >
+                    <FaSearch />
+                    Search
+                </Nav.Link>
+                <Nav.Link as={NavLink} className={`${setActive} d-flex flex-column align-items-center text-center`} to={"/"} >
+                    <FaUser />
+                    Login
+                </Nav.Link>
+                <Nav.Link as={NavLink} className={`${setActive} d-flex flex-column align-items-center text-center`} to={"/"} >
+                    <FaShoppingCart />
+                    Carrito
+                </Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+
+
+    </Navbar>
   )
 }
 
-export default Navbar
+export default NavBar
