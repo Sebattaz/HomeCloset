cb99e3ca548865734dca5164a40a07f5e334b7b3
Sebattaz <seba.lostar@gmail.com>
2024-12-09 20:41:23 -0300

    Comenzando Backend

--- frontend/src/views/Carrito.jsx
+++ frontend/src/views/Carrito.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { useContext } from 'react'
-import { Container, Button, ListGroup } from 'react-bootstrap'
+import { Container, ListGroup } from 'react-bootstrap'
+import './styles/Carrito.css';
 import { homeContext } from '../context/HomeContext'
 
 const Carrito = () => {
 
     const {carrito} = useContext(homeContext);
-
+    console.log(carrito) 
   return (
-    <Container>
+    <Container className='ctnCarrito'>
         
         <ListGroup>
           {
             carrito.map((cart,i)=>(
+                           
               <ListGroup.Item className='d-flex w-100' key={i}>
                 <div className='mx-2 d-flex w-50'>
                   <img className='imgCarrito mx-3' src={cart.img} alt={cart.nombre} />
