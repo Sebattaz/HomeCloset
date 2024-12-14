9a8f75c8e7ec29ccb8666077c272d84c803eb981
Sebattaz <seba.lostar@gmail.com>
2024-12-13 23:23:53 -0300

    Agregar usuario

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,26 @@
-const app = require('./app');
+const app = require("./app");
 
+const {
+  getAdministradores,
+  agregarUsuario,
+} = require("./src/controllers/usersControllers");
 
-const {getAdministradores} = require('./src/controllers/usersControllers')
+app.listen(process.env.PORT, () =>
+  console.log(`Servidor levantado en puerto ${process.env.PORT}`)
+);
 
+app.get("/administradores", async (req, res) => {
+  const data = await getAdministradores();
+  res.json(data);
+});
 
-    
-app.listen(process.env.PORT, 
-    ()=> console.log(`Servidor levantado en puerto ${process.env.PORT}`))
-    
+app.post("/usuario", async (req, res) => {
+  try {
+    const {rut, password, email, nombre, edad, direccion} = req.body;
 
-app.get("/administradores", async (req,res)=>{
-    const data = await getAdministradores();
-    res.json(data);
+    await agregarUsuario(rut, password, email, nombre, edad, direccion);
+    res.send("Usuario creado con éxito");
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
